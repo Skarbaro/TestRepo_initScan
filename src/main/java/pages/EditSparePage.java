da4be3065329c63212dfa713692ef220f3c611bf
Alex Romaniuk <alaster.rom@gmail.com>
2021-07-21 14:55:51 +0300

    added pages file java EditSparePage.java

---
+++ src/main/java/pages/EditSparePage.java
@@ -0,0 +1,31 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class EditSparePage extends ParentPage{
+    @FindBy(id = "spares_spareName")
+    private WebElement spareNameInput;
+
+    @FindBy(id = "spares_spareType")
+    private WebElement spareTypeDD;
+
+    @FindBy(name = "delete")
+    private WebElement buttonDelete;
+
+    @FindBy(name = "add")
+    private WebElement buttonAdd;
+
+    public EditSparePage(WebDriver webDriver){
+        super (webDriver, "/dictionary/spares/edit");
+    }
+
+    public boolean checkSpareNameInInput (String spareName){        // Получить текст и сравнить с тем который ищем
+        return spareNameInput.getAttribute("value").equals(spareName);
+    }
+
+    public void clickButtonDelete(){
+        actionsWithOurElements.clickOnElement(buttonDelete);
+    }
+}
